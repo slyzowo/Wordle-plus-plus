12a9aac811c906e6d4147422bc668e0a94fda226
slyzOwO <lilithhimes@gmail.com>
2025-08-02 15:56:48 -0500

    beginning

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+void startseq(){
+  std::cout << " Welcome to wordle but better, wordle++" << '\n';
+}
+
+int main(){
+
+  startseq();
+
+/*
+start seq
+  asks for
+  - character count
+  - chances count
+
+main game
+  gets random word from word.txt file
+  for loop of guesses 
+
+"death" seq
+  ends game, asks to play again
+*/
+
+return 0;
+}
