22e9b9ec35efe5ecb0069d4d952058c91b97c335
slyzOwO <lilithhimes@gmail.com>
2025-08-02 16:37:14 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+using std::cout;
+using std::cin;
 
 void startseq(){
   std::cout << " Welcome to wordle but better, wordle++" << '\n';
@@ -7,18 +9,37 @@ void startseq(){
 
 int main(){
 
+  int chanceCount = 0;
+  int characterCount = 0;
+
   startseq();
+  cout << "how many characters would you like in your word?" << '\n' << "0 for default" << '\n';
+  cin >> characterCount;
+  if (characterCount == 0){
+    characterCount = 5;
+  }
+
+  cout << "how many chances would you like?" << '\n' << "0 for default" << '\n';
+  cin >> chanceCount;
+  if (chanceCount == 0){
+    chanceCount = 5;
+  }
+
 
 /*
-start seq
-  asks for
-  - character count
-  - chances count
-
 main game
   gets random word from word.txt file
   for loop of guesses 
+*/
 
+for (int i = chanceCount; i >= 0; i--){
+  cout << "Chances to guess : " << i << '\n';
+}
+
+cout << "Game Over!"<< '\n';
+
+
+/*
 "death" seq
   ends game, asks to play again
 */
