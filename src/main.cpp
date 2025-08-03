8a21b2d1ac3b82e8f8ed905f6e5484967eb4a707
slyzOwO <lilithhimes@gmail.com>
2025-08-02 23:57:26 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 using std::cout;
 using std::cin;
@@ -11,15 +12,17 @@ int main(){
 
   int chanceCount = 0;
   int characterCount = 0;
+  std::string userGuess = "";
+  std::string randword = "";
 
   startseq();
-  cout << "how many characters would you like in your word?" << '\n' << "0 for default" << '\n';
+  cout << "how many characters would you like in your word?" << '\n' << "[ 0 for default ]" << '\n';
   cin >> characterCount;
   if (characterCount == 0){
     characterCount = 5;
   }
 
-  cout << "how many chances would you like?" << '\n' << "0 for default" << '\n';
+  cout << "how many chances would you like?" << '\n' << "[ 0 for default ]" << '\n';
   cin >> chanceCount;
   if (chanceCount == 0){
     chanceCount = 5;
@@ -29,11 +32,22 @@ int main(){
 /*
 main game
   gets random word from word.txt file
-  for loop of guesses 
 */
 
+// random number gen + get random word from file
+
 for (int i = chanceCount; i >= 0; i--){
+
+// main gameplay with a multidimensional array
+
   cout << "Chances to guess : " << i << '\n';
+  
+  cin >> userGuess;
+  cout << userGuess << '\n';
+
+  
+
+  
 }
 
 cout << "Game Over!"<< '\n';
