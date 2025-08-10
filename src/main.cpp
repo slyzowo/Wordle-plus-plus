6c4f1d047a332f23d324f7f7ef8e2677dfe35a50
slyzOwO <lilithhimes@gmail.com>
2025-08-09 20:47:19 -0500

    Refactor input handling and add .gitignore
    Moved chance and character selection logic into separate functions for better code organization. Added a .gitignore file to exclude .exe files from version control.

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,38 @@
 using std::cout;
 using std::cin;
 
+int chanceCount;
+int characterCount;
+
 void startseq(){
   std::cout << " Welcome to wordle but better, wordle++" << '\n';
 }
 
-int main(){
-
-  int chanceCount = 0;
-  int characterCount = 0;
-  std::string userGuess = "";
-  std::string randword = "";
-
-  startseq();
-  cout << "how many characters would you like in your word?" << '\n' << "[ 0 for default ]" << '\n';
-  cin >> characterCount;
-  if (characterCount == 0){
-    characterCount = 5;
-  }
-
+int chanceSelector(){
   cout << "how many chances would you like?" << '\n' << "[ 0 for default ]" << '\n';
   cin >> chanceCount;
   if (chanceCount == 0){
     chanceCount = 5;
   }
+return chanceCount;
+}
 
+int characterSelector(){
+  cout << "how many characters would you like in your word?" << '\n' << "[ 0 for default ]" << '\n';
+  cin >> characterCount;
+  if (characterCount == 0){
+    characterCount = 5;
+  }
+return characterCount;
+}
+
+int main(){
+  std::string userGuess = "";
+  std::string randword = "";
+
+  startseq();
+  characterSelector();
+  chanceSelector();
 
 /*
 main game
@@ -38,17 +46,13 @@ main game
 // random number gen + get random word from file
 
 for (int i = chanceCount; i >= 0; i--){
-
 // main gameplay with a multidimensional array
 
   cout << "Chances to guess : " << i << '\n';
-  
   cin >> userGuess;
-  cout << userGuess << '\n';
 
-  
+// compare secret word to users word and return results
 
-  
 }
 
 cout << "Game Over!"<< '\n';
