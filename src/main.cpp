f2682bb5ff8fc8c5029bdc5cf59d6f3df978015e
slyzOwO <lilithhimes@gmail.com>
2025-08-10 02:40:14 -0500

    Improve main.cpp and add VSCode settings
    Refactored src/main.cpp to use standard namespace imports, improved prompts, and implemented random word selection from file. Added .vscode/settings.json to associate 'string' files with C++ for better editor support.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,18 @@
 #include <fstream>
 using std::cout;
 using std::cin;
+using std::string;
+using std::ifstream;
 
 int chanceCount;
 int characterCount;
 
 void startseq(){
-  std::cout << " Welcome to wordle but better, wordle++" << '\n';
+  cout << " Welcome to wordle but better, wordle++" << '\n' << "[ please type in UPPERcase ]" << '\n' << '\n';
 }
 
 int chanceSelector(){
-  cout << "how many chances would you like?" << '\n' << "[ 0 for default ]" << '\n';
+  cout << "how many chances would you like?" << '\n' << "[ 0 for default ]" << '\n' << '\n';
   cin >> chanceCount;
   if (chanceCount == 0){
     chanceCount = 5;
@@ -21,7 +23,7 @@ return chanceCount;
 }
 
 int wordLengthSelector(){
-  cout << "how many characters would you like in your word?" << '\n' << "[ 0 for default ]" << '\n';
+  cout << "how many characters would you like in your word?" << '\n' << "[ 0 for default ]" << '\n' << '\n';
   cin >> characterCount;
   if (characterCount == 0){
     characterCount = 5;
@@ -30,27 +32,28 @@ return characterCount;
 }
 
 int main(){
-  std::string userGuess = "";
-  std::string secretWord = "";
+  string userGuess = "";
+  string secretWord = "";
+  int randnum;
 
   startseq();
   wordLengthSelector();
   chanceSelector();
 
-  std::ifstream wordlistFile("wordlist.txt");
+  ifstream file("wordlist.txt");
 
-// random number + forloop for line
+  srand(time(0));
+  int target = rand() % 5662 + 1;
 
-  while (getline(wordlistFile, secretWord)){
-    cout << "Secret Word : " << secretWord << '\n';
-  }
-
-// random number gen + get random word from file
+    for (int i = 1; i <= target; i++) {
+      getline(file, secretWord);
+    }
 
   for (int i = chanceCount; i >= 0; i--){
 // main gameplay with a multidimensional array
 
   cout << "Chances to guess : " << i << '\n';
+  cout << secretWord << '\n';
   cin >> userGuess;
 
   if (i == 0){
